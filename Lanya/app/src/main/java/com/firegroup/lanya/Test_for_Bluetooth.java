48c45ce00db430b26b8b507ee66a60aa514a2c69
Frozenmad <351549709@qq.com>
2017-11-04 11:04:24 +0800

    Refine the bluetooth test
    some small change

--- Lanya/app/src/main/java/com/firegroup/lanya/Test_for_Bluetooth.java
+++ Lanya/app/src/main/java/com/firegroup/lanya/Test_for_Bluetooth.java
@@ -1,8 +1,6 @@
 package com.firegroup.lanya;
 
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.renderscript.Int2;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
